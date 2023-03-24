0ae5a71be337a557741b52972ac7fe4b557ccab1
Charles Lao <charles.h.lao@gmail.com>
2023-03-24 20:47:41 +0800

    Add display of gameBoard array on html

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-plusplus */
+
+const gameBoard = (() => {
+    const board = [
+        "x", "o", "x", "o", "x", "o", "x", "o", "x"
+    ];
+
+    return {
+        board
+    };
+})();
+
+const displayController = ((board) => {
+
+    const tBody = document.querySelector('.table-body');
+
+    const render = () => {
+
+        let boardIndex = 0;
+
+        for (let i = 0; i < 3; i++) {
+            const row = document.createElement('tr');
+
+            for (let c = 0; c < 3; c++) {
+                const cell = document.createElement('td');
+                const moveText = document.createElement('p');
+
+
+                moveText.textContent = board[boardIndex];
+                boardIndex++;
+
+                cell.appendChild(moveText);
+                row.appendChild(cell);
+            }
+
+            tBody.appendChild(row);
+        }
+
+    };
+
+    return {
+        render
+    };
+})(gameBoard.board);
+
+displayController.render();
