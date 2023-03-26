826658c62ad4b24ddeaa2cc3476798012a800924
Charles Lao <charles.h.lao@gmail.com>
2023-03-27 00:57:43 +0800

    Add player move icon when clicking table cells

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-plusplus */
 
 const gameBoard = (() => {
     const board = [
-        "x", "o", "x", "o", "x", "o", "x", "o", "x"
+        "", "", "", "", "", "", "", "", ""
     ];
 
     return {
@@ -12,10 +14,10 @@ const gameBoard = (() => {
 
 const displayController = ((board) => {
 
-    const tBody = document.querySelector('.table-body');
+    const _tBody = document.querySelector('.table-body');
 
-    const render = () => {
-
+    const renderBoard = () => {
+        _tBody.textContent = '';
         let boardIndex = 0;
 
         for (let i = 0; i < 3; i++) {
@@ -24,28 +26,84 @@ const displayController = ((board) => {
             for (let c = 0; c < 3; c++) {
                 const cell = document.createElement('td');
                 cell.classList.add('boardCells');
+                cell.dataset.indexNumber = boardIndex;
+                cell.addEventListener('click', game.placeMove);
+                cell.textContent = board[boardIndex];
 
-                const moveText = document.createElement('p');
+                // const moveText = document.createElement('p');
 
-                cell.addEventListener('click', (e) => {
-                    alert(cell);
-                });
-
-                moveText.textContent = board[boardIndex];
+                // moveText.textContent = board[boardIndex];
                 boardIndex++;
 
-                cell.appendChild(moveText);
+                // cell.appendChild(moveText);
                 row.appendChild(cell);
             }
 
-            tBody.appendChild(row);
+            _tBody.appendChild(row);
         }
 
     };
 
     return {
-        render
+        renderBoard
     };
+
 })(gameBoard.board);
 
-displayController.render();
+
+const Player = (name, playerType) => ({
+        name, playerType
+    });
+
+const game = (() => {
+    
+    let roundNo = 1;
+    const playerX = Player('Player-X', 'x');
+    const playerO = Player('Player-O', 'o');
+    
+
+    const start = () => {
+        
+        displayController.renderBoard();
+
+    }
+
+    // get gamestate or playerturn from the gameobject or player object
+    const placeMove = (e) => {
+
+        const moveType = _checkPlayerTurn();
+
+        gameBoard.board[e.target.dataset.indexNumber] = moveType;
+        displayController.renderBoard();
+    };
+
+    const _checkPlayerTurn = () => {
+
+        let moveType;
+
+        if( (roundNo % 2) === 0){
+            moveType = playerO.playerType;
+            roundNo++;
+        } else {
+            moveType =  playerX.playerType;
+            roundNo++;
+        }
+
+        return moveType;
+    }
+
+    
+
+    return {
+        start, placeMove, roundNo, playerX, playerO
+    }
+
+})();
+
+
+
+
+
+
+game.start();
+
